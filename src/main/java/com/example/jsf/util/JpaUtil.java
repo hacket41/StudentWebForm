043feec8d78753f26849673ce0443b3371c192b4
Abhijit Singh <abhijit.singh.0106@gmail.com>
2025-08-06 23:32:57 +0545

    Database connection is successfull, data is saved and deleted; Editing remains

--- src/main/java/com/example/jsf/util/JpaUtil.java
+++ src/main/java/com/example/jsf/util/JpaUtil.java
@@ -1,3 +1,4 @@
+
 package com.example.jsf.util;
 
 import javax.persistence.EntityManager;
@@ -5,9 +6,16 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JpaUtil {
+
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("studentPU");
 
+    // Provide an EntityManager for use in DAOs
     public static EntityManager getEntityManager() {
         return emf.createEntityManager();
+    }
+
+    // Optional: close EntityManagerFactory on app shutdown
+    public static void close() {
+        emf.close();
     }
 }
